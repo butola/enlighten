effe67b8babcc46ed3b0f68187ddebf7970b00e1
ybutola <yogendra_s_butola@optum.com>
2016-02-10 10:05:55 -0600

    Added this file to verify what happens when there is one object in the list while sorting.

---
+++ src/Test.java
@@ -0,0 +1,57 @@
+package com.optum.cirrus.legacy.core.pricingengine.businesslogic.ambulance;
+
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by ybutola on 10/4/15.
+ */
+public class Test {
+    public static void main(String args[]){
+        Person person = new Person();
+        person.setAge(22);
+        person.setName("Yogi");
+
+        Person person2 = new Person();
+        person2.setAge(24);
+        person2.setName("Butola");
+        List<Person> personList = new ArrayList<Person>();
+        personList.add(person);
+      //  personList.add(person2);
+
+        Collections.sort(personList, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return Double.compare(o2.getAge(), o1.getAge());
+            }
+        });
+
+        for(Person _person: personList){
+            System.out.println("Name : "+ _person.getName() + " is eldest !!!!");
+        }
+    }
+}
+
+class Person{
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
